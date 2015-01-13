1baba1969da2f110fda6e5ffd8be0bb16752cb51
kushal <kushaldeveloper@gmail.com>
2015-01-13 14:34:25 -0600

    welp

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,61 @@ namespace ConsoleApplication1
 {
     class Program
     {
+
+        static void doit()
+        {
+            for (UInt64 i = 0; i < UInt64.MaxValue; i++)
+            {
+                double x = Math.Sqrt(Math.PI);
+                Console.WriteLine(x);
+                Random r = new Random();
+                Console.WriteLine(r);
+                Console.WriteLine("Hello, world...");
+                string urlAddress = "https://etbumath.appspot.com";
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlAddress);
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Stream receiveStream = response.GetResponseStream();
+                    StreamReader readStream = null;
+
+                    if (response.CharacterSet == null)
+                    {
+                        readStream = new StreamReader(receiveStream);
+                    }
+                    else
+                    {
+                        readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+                    }
+
+                    string data = readStream.ReadToEnd();
+
+                    response.Close();
+                    readStream.Close();
+                }
+            }
+        }
+        static void A()
+        {
+            System.Threading.Thread.Sleep(5);
+            Console.WriteLine('A');
+            doit();
+        }
+
+        static void B()
+        {
+            System.Threading.Thread.Sleep(12);
+            Console.WriteLine('B');
+            doit();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world...");
+            for (UInt64 i = 0; i < UInt64.MaxValue; i++)
+            {
+                A(); B();
+            }
             Console.ReadKey();
         }
     }
